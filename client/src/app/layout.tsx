68440924098e4b85cd0b857f8da9914656be67ed
Leonel Herrera <leonelherreradkno@gmail.com>
2024-02-25 11:01:55 -0300

    Implemento el 'force-dynamic'

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   title: 'Stock Control',
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>{children}</body>
     </html>
   );
